a7a1f4ce65463a3292d398cf515a9a88bee520fa
30275 <30275@WUHUQIFEI>
2025-11-20 23:34:04 +0800

    全局异常处理

---
+++ blog-api/src/main/java/com/itzpy/blog/handler/AllExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.itzpy.blog.handler;
+
+import com.itzpy.blog.vo.Result;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+// 统一异常处理(对所有的Controller进行AOP加强)
+@ControllerAdvice
+public class AllExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    @ResponseBody//响应给前端json数据
+    public Result doException(Exception ex) {
+        ex.printStackTrace();
+
+        return Result.fail(-999, "服务器异常，请联系管理员");
+    }
+}
